9ac66fb1dd6f5e16c57aa3774f00ca354f93bc71
The Coder Abhi <abhishekpopli80@gmail.com>
2018-03-07 17:21:16 +0530

    GPS feature updated

--- app/src/main/java/com/example/vasupc/bvp_hackathon/HomeActivity.java
+++ app/src/main/java/com/example/vasupc/bvp_hackathon/HomeActivity.java
@@ -1,47 +1,30 @@
 package com.example.vasupc.bvp_hackathon;
 
+import android.*;
+import android.Manifest;
 import android.annotation.TargetApi;
-import android.app.Service;
-import android.content.Context;
-import android.content.DialogInterface;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.icu.text.DateFormat;
 import android.location.Criteria;
-import android.os.Build;
-import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.android.gms.maps.LocationSource;
-
-import java.util.ArrayList;
-
+import com.google.android.gms.maps.model.LatLng;
 
 public class HomeActivity extends AppCompatActivity implements LocationListener {
-    final String TAG = "GPS";
-    private final static int ALL_PERMISSIONS_RESULT = 101;
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10;
-    private static final long MIN_TIME_BW_UPDATES = 1000 * 60 * 1;
 
-    TextView tvLatitude, tvLongitude, tvTime;
+    LatLng myPosition;
+    Location location;
     LocationManager locationManager;
-    Location loc;
-    ArrayList<String> permissions = new ArrayList<>();
-    ArrayList<String> permissionsToRequest;
-    ArrayList<String> permissionsRejected = new ArrayList<>();
-    boolean isGPS = false;
-    boolean isNetwork = false;
-    boolean canGetLocation = true;
-
+    private  TextView tvLatitude, tvLongitude;
+    private String latitude_value = "";
+    private String longitude_value = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,184 +32,71 @@ public class HomeActivity extends AppCompatActivity implements LocationListener 
         setContentView(R.layout.activity_home);
         tvLatitude = (TextView) findViewById(R.id.tvLatitude);
         tvLongitude = (TextView) findViewById(R.id.tvLongitude);
-        tvTime = (TextView) findViewById(R.id.tvTime);
 
-        locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
-        isGPS = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-        isNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        onMapReady();
 
-
-        if (!isGPS && !isNetwork) {
-            Log.d(TAG, "Connection off");
-            showSettingsAlert();
-            getLastLocation();
-        } else {
-            Log.d(TAG, "Connection on");
-            // check permissions
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (permissionsToRequest.size() > 0) {
-                    requestPermissions(permissionsToRequest.toArray(new String[permissionsToRequest.size()]),
-                            ALL_PERMISSIONS_RESULT);
-                    Log.d(TAG, "Permission requests");
-                    canGetLocation = false;
-                }
-            }
-
-            // get location
-            getLocation();
+        if (ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
         }
+        double latitude ;
+        double longitude;
+        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if(location!= null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+        else {
+            Toast.makeText(HomeActivity.this, "Please select your location.", Toast.LENGTH_SHORT).show();
+            return;
+
+        }
+
+        latitude_value = Double.toString(latitude);
+        longitude_value = Double.toString(longitude);
+
+
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        Log.d(TAG, "onLocationChanged");
-        updateUI(location);
+        Toast.makeText(this, ""+location.getLatitude(), Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {}
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
 
     @Override
     public void onProviderEnabled(String s) {
-        getLocation();
+
     }
 
     @Override
     public void onProviderDisabled(String s) {
-        if (locationManager != null) {
-            locationManager.removeUpdates(this);
-        }
+
     }
 
-    private void getLocation() {
-        try {
-            if (canGetLocation) {
-                Log.d(TAG, "Can get location");
-                if (isGPS) {
-                    // from GPS
-                    Log.d(TAG, "GPS on");
-                    locationManager.requestLocationUpdates(
-                            LocationManager.GPS_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+public void onMapReady() {
 
-                    if (locationManager != null) {
-                        loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        if (loc != null)
-                            updateUI(loc);
-                    }
-                } else if (isNetwork) {
-                    // from Network Provider
-                    Log.d(TAG, "NETWORK_PROVIDER on");
-                    locationManager.requestLocationUpdates(
-                            LocationManager.NETWORK_PROVIDER,
-                            MIN_TIME_BW_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
-                    if (locationManager != null) {
-                        loc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                        if (loc != null)
-                            updateUI(loc);
-                    }
-                } else {
-                    loc.setLatitude(0);
-                    loc.setLongitude(0);
-                    updateUI(loc);
-                }
-            } else {
-                Log.d(TAG, "Can't get location");
-            }
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        }
-    }
+        // Creating a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
 
-    private void getLastLocation() {
-        try {
-            Criteria criteria = new Criteria();
-            String provider = locationManager.getBestProvider(criteria, false);
-            Location location = locationManager.getLastKnownLocation(provider);
-            Log.d(TAG, provider);
-            Log.d(TAG, location == null ? "NO LastLocation" : location.toString());
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        }
-    }
 
-    private ArrayList findUnAskedPermissions(ArrayList<String> wanted) {
-        ArrayList result = new ArrayList();
+        // Getting the name of the best provider
+        String provider = locationManager.getBestProvider(criteria, true);
 
-        for (String perm : wanted) {
-            if (!hasPermission(perm)) {
-                result.add(perm);
-            }
+        // Getting Current Location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        // TODO: Consider calling;
+        return;
         }
 
-        return result;
-    }
+        location = locationManager.getLastKnownLocation(provider);
 
-    private boolean hasPermission(String permission) {
-        if (canAskPermission()) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                return (checkSelfPermission(permission) == PackageManager.PERMISSION_GRANTED);
-            }
+
         }
-        return true;
-    }
 
-    private boolean canAskPermission() {
-        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
-    }
-
-    @TargetApi(Build.VERSION_CODES.M)
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case ALL_PERMISSIONS_RESULT:
-                Log.d(TAG, "onRequestPermissionsResult");
-                for (String perms : permissionsToRequest) {
-                    if (!hasPermission(perms)) {
-                        permissionsRejected.add(perms);
-                    }
-                }
-
-                break;
         }
-    }
-
-    public void showSettingsAlert() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setTitle("GPS is not Enabled!");
-        alertDialog.setMessage("Do you want to turn on GPS?");
-        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                startActivity(intent);
-            }
-        });
-
-        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-
-        alertDialog.show();
-    }
-
-
-    private void updateUI(Location loc) {
-        Log.d(TAG, "updateUI");
-        tvLatitude.setText(Double.toString(loc.getLatitude()));
-        tvLongitude.setText(Double.toString(loc.getLongitude()));
-        tvTime.setText(DateFormat.getTimeInstance().format(loc.getTime()));
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (locationManager != null) {
-            locationManager.removeUpdates(this);
-        }
-    }
-}
