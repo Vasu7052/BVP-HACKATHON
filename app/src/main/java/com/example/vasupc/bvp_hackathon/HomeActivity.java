79a38d4d9fa2089edfc00cbf5a251b095b6028db
The Coder Abhi <abhishekpopli80@gmail.com>
2018-03-07 20:45:44 +0530

    GeoCoder added

--- app/src/main/java/com/example/vasupc/bvp_hackathon/HomeActivity.java
+++ app/src/main/java/com/example/vasupc/bvp_hackathon/HomeActivity.java
@@ -4,99 +4,154 @@ import android.*;
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.maps.model.LatLng;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class HomeActivity extends AppCompatActivity implements LocationListener {
 
-    LatLng myPosition;
-    Location location;
-    LocationManager locationManager;
-    private  TextView tvLatitude, tvLongitude;
-    private String latitude_value = "";
-    private String longitude_value = "";
-
+    private TextView latitudePosition;
+    private TextView longitudePosition;
+    private TextView currentCity;
+    private LocationManager locationManager;
+    private Location location;
+    private final int REQUEST_LOCATION = 200;
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        tvLatitude = (TextView) findViewById(R.id.tvLatitude);
-        tvLongitude = (TextView) findViewById(R.id.tvLongitude);
-
-        onMapReady();
-
-        if (ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+        latitudePosition = (TextView) findViewById(R.id.latitude);
+        longitudePosition = (TextView) findViewById(R.id.longitude);
+        currentCity = (TextView) findViewById(R.id.city);
+        locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(HomeActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 2, this);
+            if (locationManager != null) {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
         }
-        double latitude ;
-        double longitude;
-        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        if(location!= null) {
-            latitude = location.getLatitude();
-            longitude = location.getLongitude();
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            if (location != null) {
+                latitudePosition.setText(String.valueOf(location.getLatitude()));
+                longitudePosition.setText(String.valueOf(location.getLongitude()));
+                getAddressFromLocation(location, getApplicationContext(), new GeoCoderHandler());
+            }
+        } else {
+            showGPSDisabledAlertToUser();
         }
-        else {
-            Toast.makeText(HomeActivity.this, "Please select your location.", Toast.LENGTH_SHORT).show();
-            return;
-
-        }
-
-        latitude_value = Double.toString(latitude);
-        longitude_value = Double.toString(longitude);
-
-
     }
-
     @Override
     public void onLocationChanged(Location location) {
-        Toast.makeText(this, ""+location.getLatitude(), Toast.LENGTH_SHORT).show();
+        latitudePosition.setText(String.valueOf(location.getLatitude()));
+        longitudePosition.setText(String.valueOf(location.getLongitude()));
+        getAddressFromLocation(location, getApplicationContext(), new GeoCoderHandler());
     }
-
     @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-
+    public void onStatusChanged(String provider, int status, Bundle extras) {
     }
-
     @Override
-    public void onProviderEnabled(String s) {
-
+    public void onProviderEnabled(String provider) {
     }
-
     @Override
-    public void onProviderDisabled(String s) {
-
+    public void onProviderDisabled(String provider) {
+        if (provider.equals(LocationManager.GPS_PROVIDER)) {
+            showGPSDisabledAlertToUser();
+        }
     }
-
-public void onMapReady() {
-
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        // Creating a criteria object to retrieve provider
-        Criteria criteria = new Criteria();
-
-
-        // Getting the name of the best provider
-        String provider = locationManager.getBestProvider(criteria, true);
-
-        // Getting Current Location
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-        // TODO: Consider calling;
-        return;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            }
         }
-
-        location = locationManager.getLastKnownLocation(provider);
-
-
+    }
+    private void showGPSDisabledAlertToUser() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("GPS is disabled in your device. Would you like to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Goto Settings Page To Enable GPS", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Intent callGPSSettingIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(callGPSSettingIntent);
+                    }
+                });
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
+    public static void getAddressFromLocation(final Location location, final Context context, final Handler handler) {
+        Thread thread = new Thread() {
+            @Override public void run() {
+                Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+                String result = null;
+                try {
+                    List<Address> list = geocoder.getFromLocation(
+                            location.getLatitude(), location.getLongitude(), 1);
+                    if (list != null && list.size() > 0) {
+                        Address address = list.get(0);
+                        // sending back first address line and locality
+                        result = address.getAddressLine(0) + ", " + address.getLocality() + ", " +  address.getCountryName() ;
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Impossible to connect to Geocoder", e);
+                } finally {
+                    Message msg = Message.obtain();
+                    msg.setTarget(handler);
+                    if (result != null) {
+                        msg.what = 1;
+                        Bundle bundle = new Bundle();
+                        bundle.putString("address", result);
+                        msg.setData(bundle);
+                    } else
+                        msg.what = 0;
+                    msg.sendToTarget();
+                }
+            }
+        };
+        thread.start();
+    }
+    private class GeoCoderHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            String result;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    result = bundle.getString("address");
+                    break;
+                default:
+                    result = null;
+            }
+            currentCity.setText(result);
         }
-
-        }
+    }
+}
